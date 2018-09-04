b1729384f50d72da3a2e7b42c74c9ae6a4b8c3c4
draguo <draguo@sina.com>
2018-09-04 17:21:35 +0800

    修改实现 api

---
+++ src/Core/Application.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Draguo\Dingtalk\Core;
+
+use Draguo\Dingtalk\Contacts\User;
+use Draguo\Dingtalk\QrConnect\Socialite;
+use Draguo\Dingtalk\Supports\Config;
+
+class Application
+{
+    protected $config;
+
+    public function __construct($config)
+    {
+        $this->config = new Config($config);
+    }
+
+    public function socialite($app)
+    {
+        $appId = $this->config->get("socialite.{$app}.appid");
+        $appSecret = $this->config->get("socialite.{$app}.appserect");
+        return new Socialite($appId, $appSecret);
+    }
+
+    public function user()
+    {
+        $appId = $this->config->get('corpid');
+        $appSecret = $this->config->get('corpsecret');
+        return new User($appId, $appSecret);
+    }
+}
