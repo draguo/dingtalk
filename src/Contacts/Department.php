342bdeaab634b93b22f9f20925c03359838c634b
draguo <draguo@sina.com>
2019-12-13 15:50:36 +0800

    递归获取全部部门

--- src/Contacts/Department.php
+++ src/Contacts/Department.php
@@ -10,12 +10,29 @@ use Draguo\Dingtalk\Core\BaseClient;
 
 class Department extends BaseClient
 {
-    public function all()
+    public function all($ids = [1])
+    {
+        $departments = [];
+
+        foreach ($ids as $key => $department) {
+            $r = $this->find($department);
+            unset($ids[$key]);
+            $departments = array_merge($departments, $r);
+            $ids = array_column($r, 'id');
+            if (!empty($ids)) {
+                $departments = array_merge($departments, $this->all($ids));
+            }
+        }
+
+        return $departments;
+    }
+
+    public function find($id = 1)
     {
         return $this->get('department/list', [
             'access_token' => $this->getAccessToken(),
             'fetch_child' => true,
-            'id' => 1
+            'id' => $id
         ])['department'];
     }
 }
